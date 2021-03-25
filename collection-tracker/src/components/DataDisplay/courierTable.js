d2db18bc827abafcb1619861b2dcebe5359930ca
Joseph Mitchell <mitch137@coventry.ac.uk>
2021-03-25 15:04:36 +0000

    added courier template table

---
+++ collection-tracker/src/components/DataDisplay/courierTable.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+
+import colourTheme from '../../styles/theme'
+
+
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+
+const useRowStyles = makeStyles({
+  root: {
+    '& > *': {
+      borderBottom: 'unset',
+    },
+  },
+});
+
+function createData(name, calories, fat, carbs, protein, price) {
+  return {
+    name,
+    calories,
+    fat,
+    carbs,
+    protein,
+    price,
+    history: [
+      { date: '2020-01-05', customerId: '11091700', amount: 3 },
+      { date: '2020-01-02', customerId: 'Anonymous', amount: 1 },
+    ],
+  };
+}
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+  const classes = useRowStyles();
+
+  return (
+    <React.Fragment>
+      <TableRow className={classes.root}>
+        <TableCell>
+          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell>{row.calories}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Typography variant="h6" gutterBottom component="div">
+                Courier Details
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Tracking No.</TableCell>
+                    <TableCell>Address</TableCell>
+                    <TableCell>Postcode</TableCell>
+                    <TableCell>Date Posted</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.history.map((historyRow) => (
+                    <TableRow key={historyRow.date}>
+                      <TableCell component="th" scope="row">
+                        {historyRow.date}
+                      </TableCell>
+                      <TableCell>{historyRow.customerId}</TableCell>
+                      <TableCell>{historyRow.amount}</TableCell>
+                      <TableCell>
+                        {Math.round(historyRow.amount * row.price * 100) / 100}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159),
+  createData('Ice cream sandwich', 237),
+  createData('Eclair', 262),
+  createData('Cupcake', 305),
+  createData('Gingerbread', 356),
+];
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: colourTheme.primary.main,
+    color: theme.palette.common.white,
+    fontWeight: 700,
+    flex: 1,
+  },
+  body: {
+    fontSize: 14,
+    fontWeight: 700,
+  },
+}))(TableCell);
+
+export default function CourierTable() {
+  return (
+    <TableContainer>
+      <Table aria-label="collapsible table" size='small'>
+        <TableHead>
+          <TableRow style={{backgroundColor: colourTheme.primary.main}}>
+            <StyledTableCell />
+            <StyledTableCell>Courier</StyledTableCell>
+            <StyledTableCell fullWidth>Undelivered Packages</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <Row key={row.name} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- collection-tracker/src/components/pages/managerHome.js
+++ collection-tracker/src/components/pages/managerHome.js
@@ -1,9 +1,144 @@
 import React from 'react'
 
+import Paper from '@material-ui/core/Paper'
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+
+import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
+import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
+
+import colourTheme from '../../styles/theme';
+
+import CourierTable from '../DataDisplay/courierTable';
+
+
+const StyledTabs = withStyles({
+  indicator: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    '& > span': {
+      maxWidth: 40,
+      width: '100%',
+      backgroundColor: colourTheme.primary.main,
+    },
+  },
+})((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
+
+const StyledTab = withStyles((theme) => ({
+  root: {
+    textTransform: 'none',
+    color: 'white',
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(15),
+    '&:focus': {
+      opacity: 1,
+    },
+  },
+}))((props) => <Tab disableRipple {...props} />);
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  padding: {
+    padding: theme.spacing(3),
+  },
+  tabHeader: {
+    backgroundColor: theme.palette.background.paper,
+    margin: theme.spacing(1),
+    maxWidth: '100%',
+    justifyContent: 'center',
+  },
+  tabText: {
+    color: '#292929',
+  },
+  title: {
+    fontSize: '3em',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '1.8em',
+    }
+  },
+  intro: {
+    flex: '0 0 10%',
+    position: "relative",
+    boxSizing: 'border-box',
+    textAlign: 'left',
+    color: colourTheme.text.main,
+  },
+  subText: {
+    fontSize: '1.5em',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '1em',
+    }
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+    height: '100%',
+  },
+}));
+
+
 export default function ManagerHome({token}) {
+
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>
-      <h1>Manager</h1>
+    <>
+    <div className={classes.intro}>
+      <h1 className={classes.title}>Manager Homepage</h1>
     </div>
+    <Paper>
+      <Grid>
+        <Grid item xs={12}>
+          <div className={`${classes.tabHeader} ${classes.sectionDesktop}`}>
+            <StyledTabs 
+              value={value} 
+              onChange={handleChange} 
+              aria-label="styled tabs example">
+              <StyledTab icon={<LocalShippingIcon />} className={classes.tabText} label="Couriers" />
+              <StyledTab icon={<QueryBuilderIcon />} className={classes.tabText} label="Idle Packages" />
+              <StyledTab icon={<AssignmentTurnedInIcon />} className={classes.tabText} label="Delivered Packages" />
+            </StyledTabs>
+            <Typography className={classes.padding} />
+          </div>
+          <div className={`${classes.tabHeader} ${classes.sectionMobile}`}>
+            <StyledTabs fullWidth variant="fullWidth" value={value} onChange={handleChange} aria-label="styled tabs example">
+              <StyledTab icon={<LocalShippingIcon />} className={classes.tabText}/>
+              <StyledTab icon={<QueryBuilderIcon />} className={classes.tabText}/>
+              <StyledTab icon={<AssignmentTurnedInIcon />} className={classes.tabText}/>
+            </StyledTabs>
+            <Typography className={classes.padding} />
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <Container>
+            <CourierTable />
+          </Container>
+        </Grid>
+      </Grid>
+      
+      
+    </Paper>
+    </>
   )
 }
